df22b81e577491455d49e4598244d2141cb3f792
napalm255 <napalm255@gmail.com>
2015-11-30 01:45:09 -0500

    misc

--- src/app/controllers/VisitorsController.js
+++ src/app/controllers/VisitorsController.js
@@ -9,20 +9,20 @@
         var vm = this;
 
         // TODO: move data to the service
-        vm.visitorsChartData = [ {key: 'Mobile', y: 5264}, { key: 'Desktop', y: 3872} ];
+        vm.visitorsChartData = [ {key: 'Open', y: 10}, {key: 'Approved / Not Implemented', y: 5}, { key: 'Needs Verifier', y: 6} ];
 
         vm.chartOptions = {
             chart: {
                 type: 'pieChart',
                 height: 210,
-                donut: true,
+                donut: false,
                 x: function (d) { return d.key; },
                 y: function (d) { return d.y; },
                 valueFormat: (d3.format(".0f")),
-                color: ['rgb(0, 150, 136)', '#E75753'],
+                color: ['#f00', '#0f0', '#00f'],
                 showLabels: false,
                 showLegend: false,
-                title: 'Over 9K',
+                title: '',
                 margin: { top: -10 }
             }
         };
--- src/app/index.js
+++ src/app/index.js
@@ -43,42 +43,15 @@ angular.module('angularMaterialAdmin', ['ngAnimate', 'ngCookies', 'ngTouch',
 
     $mdThemingProvider
       .theme('default')
-        .primaryPalette('indigo', {
-          'default': '600'
+        .primaryPalette('blue', {
+          'default': '900'
         })
-        .accentPalette('indigo', {
-          'default': '500'
+        .accentPalette('blue', {
+          'default': '800'
         })
-        .warnPalette('red');
-
-    $mdThemingProvider.theme('dark', 'default')
-      .primaryPalette('defaultPrimary')
-      .dark();
-
-    $mdThemingProvider.theme('grey', 'default')
-      .primaryPalette('grey');
-
-    $mdThemingProvider.theme('custom', 'default')
-      .primaryPalette('defaultPrimary', {
-        'hue-1': '50'
-    });
-
-    $mdThemingProvider.definePalette('defaultPrimary', {
-      '50':  '#FFFFFF',
-      '100': 'rgb(255, 198, 197)',
-      '200': '#E75753',
-      '300': '#E75753',
-      '400': '#E75753',
-      '500': '#E75753',
-      '600': '#E75753',
-      '700': '#E75753',
-      '800': '#E75753',
-      '900': '#E75753',
-      'A100': '#E75753',
-      'A200': '#E75753',
-      'A400': '#E75753',
-      'A700': '#E75753'
-    });
+        .warnPalette('red', {
+          'default': '900'
+        });
 
     $mdIconProvider.icon('user', 'assets/images/user.svg', 64);
   });
